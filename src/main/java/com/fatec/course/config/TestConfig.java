d14e09ab6ff03b5ba0f295f50c53ea30c4414088
Talita-S <talitassantos2015@gmail.com>
2021-10-25 16:58:34 -0300

    Payment, one-to-one association

--- src/main/java/com/fatec/course/config/TestConfig.java
+++ src/main/java/com/fatec/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fatec.course.entities.Category;
 import com.fatec.course.entities.Order;
 import com.fatec.course.entities.OrderItem;
+import com.fatec.course.entities.Payment;
 import com.fatec.course.entities.Product;
 import com.fatec.course.entities.User;
 import com.fatec.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+	
+		Payment pay1 = new Payment(null, Instant.parse("2021-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
